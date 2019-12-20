c515b1256b5c82de5159ca9dd07d92ca0d35e825
Jacob Grace <51347546+jagrace418@users.noreply.github.com>
2019-12-19 23:58:57 -0600

    Updated image card to resize better. Updated links to be the right color (#5)

--- resources/js/components/base/ImageCard.vue
+++ resources/js/components/base/ImageCard.vue
@@ -3,9 +3,9 @@
 		<h3 class="text-left mb-2">
 			<slot name="title"/>
 		</h3>
-		<div class="flex items-center">
+		<div class="flex items-center  flex-col">
 			<slot class="flex-1" name="content"/>
-			<div class="flex-2">
+			<div class="flex-2 max-w-2xs md:max-w-xs">
 				<img class="rounded-full" :src="imgUrl" alt="Image"/>
 			</div>
 		</div>
